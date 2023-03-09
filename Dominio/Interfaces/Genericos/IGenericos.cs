20589fdfec306d03e690540c99586f20d88d9605
Maria <mariaed.cassiano@gmail.com>
2023-03-08 21:55:07 -0300

    Criando Interfaces Genericas e de Serviço

---
+++ Dominio/Interfaces/Genericos/IGenericos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Genericos
+{
+    public interface IGenericos<T> where T : class
+    {
+        Task Adicionar(T Objeto);
+        Task<T> Atualizar(T Objeto);
+        Task<T> Excluir(T Objeto);
+        Task<T> BuscarPorId(int Id);
+        Task<List<T>> Listar();
+    }
+}
