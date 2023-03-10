8656068a7a51caa5f1498f29fe742270e0b2857e
Maria <mariaed.cassiano@gmail.com>
2023-03-09 22:01:55 -0300

    Criando camada de aplicação para disponibilizar os métodos de dominio para nossa API

---
+++ Aplicacao/Interfaces/IAplicacaoUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Interfaces
+{
+    public interface IAplicacaoUsuario
+    {
+        Task<bool> AdicionarUsuario(string email, string senha, int idade, string celular);
+    }
+}
